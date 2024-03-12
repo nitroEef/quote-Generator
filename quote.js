5352b4bf6dbd4b1723342a6ac9ad6cedfe74a841
nitroEef <mikebodetelli@gmail.com>
2024-03-12 15:34:08 +0100

    unf

---
+++ quote.js
@@ -0,0 +1,36 @@
+const quoteCon = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorName = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote')
+
+//https://type.fit/api/quotes
+let apiQuote = [];
+
+
+//to be able to get new quote
+function newQuote(){
+    const quote = apiQuote[Math.floor(Math.random() * apiQuote.length)]
+    // console.log(quote)
+
+    authorName.textContent = quote.author
+    quoteText.textContent = quote.text
+}
+
+
+
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+
+    try{
+        const request = await fetch(apiUrl);
+         apiQuote = await response.json()
+        console.log(apiQuote)
+
+        newQuote()
+
+    }catch(error){
+        console.log(error)
+    }
+}
+getQuotes()
