2afeb12e5551ca031b2cb63a0295a66caeaff544
nitroEef <mikebodetelli@gmail.com>
2024-03-12 16:36:24 +0100

    updd

--- quote.js
+++ quote.js
@@ -1,47 +1,116 @@
-const quoteCon = document.getElementById('quote-container');
+// const quoteCon = document.getElementById('quote-container');
+
+// const quoteText = document.getElementById('quote');
+
+// const authorName = document.getElementById('author');
+
+// const twitterBtn = document.getElementById('twitter');
+// const newQuoteBtn = document.getElementById('new-quote');
+
+// // ("https://type.fit/api/quotes")
+
+// let apiQuote = [];
+// function newQuote() {
+//     const quote = apiQuote[Math.floor(Math.random() * apiQuote.length)];
+//     console.log(quote);
+
+//     if(!quote.author){                          //in case the author name is not available or written
+//         authorName.textContent = 'unknown'
+//     }else {
+//         authorName.textContent =quote.author
+//     }
+
+//     // Check quote lenght to determing the styling
+
+//     if(quote.text.length > 50) {
+//         quoteText.classList.remove('long-quote')
+//     }
+//     quoteText.textContent = quote.Text
+
+//     authorName.textContent = quote.author
+//     quoteText.textContent = quote.text
+// }
+
+
+
+// async function getQuotes() {
+//     const apiUrl = 'https://type.fit/api/quotes';
+
+//     try {
+//         const response = await fetch(apiUrl);
+//          apiQuote = await response.json()
+
+//         console.log(apiQuote)
+
+//     newQuote()
+
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// //event listener
+
+// newQuoteBtn.addEventListener('click', newQuote)
+// getQuotes()
+
+
+
+// DOM 
+const quotCon = document.getElementById('quote-container');
 
 const quoteText = document.getElementById('quote');
 
 const authorName = document.getElementById('author');
 
 const twitterBtn = document.getElementById('twitter');
+
 const newQuoteBtn = document.getElementById('new-quote');
 
-// ("https://type.fit/api/quotes")
+
+// https://type.fit/api/quotes
+//**** TO GENRATE NEW QUOTE( new QUOTE from our API)******//
 
 let apiQuote = [];
-
 
 function newQuote() {
     const quote = apiQuote[Math.floor(Math.random() * apiQuote.length)];
     console.log(quote);
 
-    if(!quote.author){
-        author.Name.textContent = 'unknown'
-    }else{
-        authorName.textContent = quote.text
+    if(!quote.author){                          //in case the author name is not available or written
+        authorName.textContent = 'unknown'
+    }else {
+        authorName.textContent =quote.author
     }
+
+    // Check quote lenght to determing the styling
+
+    if(quote.text.length > 50) {
+        quoteText.classList.remove('long-quote')
+    }
+    quoteText.textContent = quote.Text
 
     authorName.textContent = quote.author
     quoteText.textContent = quote.text
 }
 
-async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes';
+async function getQuote() {
+    const apiUrl = "https://type.fit/api/quotes"
 
     try {
         const response = await fetch(apiUrl);
-         apiQuote = await response.json()
+        apiQuote = await response.json()
 
         console.log(apiQuote)
 
-    newQuote()
+        newQuote()
 
-    } catch (error) {
+
+    }catch(error) {
         console.log(error)
     }
 }
-//event listener
 
-newQuoteBtn.addEventListener('click',newQuote)
-getQuotes()
+// Event Listener   (so that when we click on new quote button, it will bring new quote)
+newQuoteBtn.addEventListener('click', newQuote)
+
+getQuote() 
