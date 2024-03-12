e7b26b431539c35ac4e7cabe916fe4bb23ba8918
nitroEef <mikebodetelli@gmail.com>
2024-03-12 16:07:19 +0100

    fjnf

--- quote.js
+++ quote.js
@@ -1,36 +1,47 @@
 const quoteCon = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorName = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote')
 
-//https://type.fit/api/quotes
+const quoteText = document.getElementById('quote');
+
+const authorName = document.getElementById('author');
+
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+// ("https://type.fit/api/quotes")
+
 let apiQuote = [];
 
 
-//to be able to get new quote
-function newQuote(){
-    const quote = apiQuote[Math.floor(Math.random() * apiQuote.length)]
-    // console.log(quote)
+function newQuote() {
+    const quote = apiQuote[Math.floor(Math.random() * apiQuote.length)];
+    console.log(quote);
+
+    if(!quote.author){
+        author.Name.textContent = 'unknown'
+    }else{
+        authorName.textContent = quote.text
+    }
 
     authorName.textContent = quote.author
     quoteText.textContent = quote.text
 }
 
-
-
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
 
-    try{
-        const request = await fetch(apiUrl);
+    try {
+        const response = await fetch(apiUrl);
          apiQuote = await response.json()
+
         console.log(apiQuote)
 
-        newQuote()
+    newQuote()
 
-    }catch(error){
+    } catch (error) {
         console.log(error)
     }
 }
+//event listener
+
+newQuoteBtn.addEventListener('click',newQuote)
 getQuotes()
